0f9a0f1f56fc2e83d3a8e2ea069fbb6d30678616
KacperDyrek <62343165+KacperDyrek@users.noreply.github.com>
2024-04-17 18:36:43 +0200

    Add tracking control

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,35 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Swiper Gallery Test', function () {
+  it('Checks if user can swipe through slides using keys', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').next().focused();
+    // cy.focused();
+    cy.wait(2000);
+    cy.get('.swiper-button-prev').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').next().focused();
+    // cy.focused();
+  });
+});
+
+describe('Swiper Gallery Slide Description Test', function () {
+  it('Verifies if description of each slide is displayed correctly', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-slide').each(($slide, index) => {
+      cy.wrap($slide).find('title');
+      cy.wrap($slide).find('.card-description').should('be.visible');
+    });
+
+    cy.get('.swiper-slide').each(($slide, index) => {
+      const expectedTitle = getExpectedTitle(index); 
+      const expectedDescription = getExpectedDescription(index); 
+      cy.wrap($slide).find('title');
+      cy.wrap($slide).find('.card-description').should('contain', expectedDescription);
+    });
+  });
+});
